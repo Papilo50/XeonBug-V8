0470c3d5bf77a7dc6830e07d00c3f0791c4f5dc0
Papilo50 <botemmanuel951@gmail.com>
2024-11-08 12:04:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349021424888"
+global.ownername = "Louis Vuitton"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349021424888'  //creator number
+global.ownername = 'louis Vuitton' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
